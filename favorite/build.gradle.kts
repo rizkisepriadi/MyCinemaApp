50122ab1966d01c4477dc31fb3cf5ac754535d3d
Rizki Sepriadi <muhammadrizkisepriadi@gmail.com>
2025-03-07 14:05:21 +0700

    feat: add dynamic feature dependency

---
+++ favorite/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    alias(libs.plugins.android.dynamic.feature)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.ksp)
+}
+android {
+    namespace = "com.app.mycinemaapp.favorite"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 24
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation(project(":core"))
+    implementation(project(":app"))
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,3 +22,4 @@ dependencyResolutionManagement {
 rootProject.name = "MyCinemaApp"
 include(":app")
 include(":core")
+include(":favorite")
