846a4a20f4591abc809c3e05373df3ee0645e6da
Rizki Sepriadi <muhammadrizkisepriadi@gmail.com>
2025-03-07 13:58:24 +0700

    change jvmTarget and javaVersion

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.app.mycinemaapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.app.mycinemaapp"
@@ -27,12 +27,16 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
+    buildFeatures {
+        viewBinding = true
+    }
+    dynamicFeatures += setOf(":favorite")
 }
 
 dependencies {
